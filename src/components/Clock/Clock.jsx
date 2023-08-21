487a98cdb1fc557911ad16ae31896e8f3d145481
basesnel <basesnel@gmail.com>
2023-08-21 21:33:53 +0300

    useRef and useEffect hook details on clock page

--- src/components/Clock/Clock.jsx
+++ src/components/Clock/Clock.jsx
@@ -1,28 +1,48 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 import { Text } from 'components/Text/Text';
 
 export const Clock = () => {
   const [time, setTime] = useState(() => new Date());
 
-  let intervalId = null;
+  const intervalId = useRef(null);
 
   useEffect(() => {
-    intervalId = setInterval(() => {
+    intervalId.current = setInterval(() => {
       console.log('This interval every 1000ms ' + Date.now());
       setTime(new Date());
     }, 1000);
+
+    return () => {
+      console.log('This is function for clear to next effect trigger');
+      stop();
+    };
   }, []);
 
+  // this is example for clear effect by return () => {func}
+  // useEffect(() => {
+  // setInterval(() => {
+  //   console.log('This interval every 3000ms ' + Date.now());
+  //   setTime(new Date());
+  // }, 3000);
+
+  //   console.log(time);
+
+  //   return () => {
+  //     console.log('This is function for clear to next effect trigger');
+  //   };
+  // }, []);
+
   const stop = () => {
-    clearInterval(intervalId);
+    clearInterval(intervalId.current);
   };
 
-  console.log(intervalId);
+  // console.log(intervalId.current);
 
   return (
     <>
       <Text>Current time: {time.toLocaleTimeString()}</Text>
+      <button onClick={() => setTime(new Date())}>Update time state</button>
       <button onClick={stop}>Stop</button>
     </>
   );

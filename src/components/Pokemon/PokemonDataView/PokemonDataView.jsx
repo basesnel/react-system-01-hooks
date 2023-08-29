6cc533b57bca2c90ee01f435cab0f34b639d371b
basesnel <basesnel@gmail.com>
2023-08-29 16:44:03 +0300

    Pokemon - minimal fil list of pokemon stats.

--- src/components/Pokemon/PokemonDataView/PokemonDataView.jsx
+++ src/components/Pokemon/PokemonDataView/PokemonDataView.jsx
@@ -1,6 +1,8 @@
 import { DecoratedImg } from 'components/DecoratedImg/DecoratedImg';
 import { Title } from 'components/Title/Title';
 
+import css from './PokemonDataView.module.css';
+
 export const PokemonDataView = ({ pokemon }) => {
   const { sprites, name, stats } = pokemon;
 
@@ -10,14 +12,16 @@ export const PokemonDataView = ({ pokemon }) => {
         src={sprites.other['official-artwork'].front_default}
         name={name}
       />
-      <Title level={3} caption={name} />
-      <ul>
-        {stats.map(entry => (
-          <li key={entry.stat.name}>
-            {entry.stat.name}: {entry.base_stat}
-          </li>
-        ))}
-      </ul>
+      <div className={css.thumb}>
+        <Title level={3} caption={name} />
+        <ul>
+          {stats.map(entry => (
+            <li key={entry.stat.name}>
+              {entry.stat.name}: {entry.base_stat}
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };

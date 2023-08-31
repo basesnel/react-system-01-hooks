6748078af14b8220e56e231c5c113d5d88ae8125
basesnel <basesnel@gmail.com>
2023-08-31 20:46:49 +0300

    Use SearchBar as common component

--- src/components/Pokemon/Pokemon.jsx
+++ src/components/Pokemon/Pokemon.jsx
@@ -1,7 +1,7 @@
 // import { PokemonForm } from 'components/Pokemon/PokemonForm/PokemonForm';
 import { PokemonInfo } from 'components/Pokemon/PokemonInfo/PokemonInfo';
 import { useState } from 'react';
-import { SearchBar } from './SearchBar/SearchBar';
+import { SearchBar } from 'components/SearchBar/SearchBar';
 
 export const Pokemon = () => {
   const [pokemonName, setPokemonName] = useState('');

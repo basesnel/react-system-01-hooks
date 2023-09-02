0c9ea1fb741ce0d189bc7e45a1e361ea319dadd7
basesnel <basesnel@gmail.com>
2023-09-02 22:06:31 +0300

    Add some props to components

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -1,5 +1,11 @@
+import PropTypes from 'prop-types';
+
 import css from './Box.module.css';
 
 export default function Box({ children }) {
   return <section className={css.box}>{children}</section>;
 }
+
+Box.propTypes = {
+  children: PropTypes.element.isRequired,
+};
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,6 +1,13 @@
+import PropTypes from 'prop-types';
+
 import css from './Container.module.css';
 
 export default function Container({ variant = null, children }) {
   const variation = variant ? `${css[variant]}` : '';
   return <div className={`${css.container} ${variation}`}>{children}</div>;
 }
+
+Container.propTypes = {
+  variant: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};

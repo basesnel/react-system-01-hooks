eb15acaef29a51b077bdf2dcd610d19b0d86af27
basesnel <basesnel@gmail.com>
2023-09-06 21:11:27 +0300

    News - first news list render

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -1,14 +1,46 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import SearchBar from 'components/SearchBar';
 
+axios.defaults.headers.common['Authorization'] =
+  'Bearer 12bb4d5829d14b34ac0d67e4ed8ca6bf';
+
+const fetchArticles = ({
+  searchQuery = '',
+  currentPage = 1,
+  pageSize = 5,
+} = {}) => {
+  return axios
+    .get(
+      `https://newsapi.org/v2//everything?q=${searchQuery}&pageSize=${pageSize}&page=${currentPage}`
+    )
+    .then(response => response.data.articles);
+};
+
 export default function News() {
   const [newQuery, setNewQuery] = useState('');
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    fetchArticles({ searchQuery: 'css' }).then(responseArticles =>
+      setArticles(responseArticles)
+    );
+  }, []);
 
   return (
     <>
       <SearchBar onFormSubmit={setNewQuery} query="for news" />
       <div>{newQuery}</div>
+      <ul>
+        {articles.map(({ title, url }) => (
+          <li key={title}>
+            <a href={url} target="_blank" rel="noopener noreferrer">
+              {title}
+            </a>
+          </li>
+        ))}
+      </ul>
     </>
   );
 }

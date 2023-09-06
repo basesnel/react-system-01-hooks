76e884a743f67495e3f2c8caf70dce634b6f8d63
basesnel <basesnel@gmail.com>
2023-09-06 15:27:24 +0300

    Character list some style add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './components/App';
 import AuthProvider from './contexts/auth/Provider';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(

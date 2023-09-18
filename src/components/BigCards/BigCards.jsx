48ae8e398eacc95f407f332da4d519d6af96cadd
basesnel <basesnel@gmail.com>
2023-09-18 10:31:05 +0300

    Big cards: I variant - modal in parent component

--- src/components/BigCards/BigCards.jsx
+++ src/components/BigCards/BigCards.jsx
@@ -1,3 +1,47 @@
-export default function Box() {
-  return <section>List of big cards...</section>;
+import { useState } from 'react';
+
+const Modal = ({ url }) => {
+  return (
+    <div>
+      <p>Modal</p>
+      <p>url: {url}</p>
+    </div>
+  );
+};
+
+const List = ({ items, onSelect }) => {
+  return (
+    <div>
+      {items.map(item => (
+        <ListItem key={item.large} item={item} onSelect={onSelect} />
+      ))}
+    </div>
+  );
+};
+
+const ListItem = ({ item, onSelect }) => {
+  return <div onClick={() => onSelect(item.large)}>{item.large}</div>;
+};
+
+export default function BigCards() {
+  // const [images, setImages] = useState([
+  //   { preview: 'preview-1', large: 'large-1' },
+  //   { preview: 'preview-2', large: 'large-2' },
+  //   { preview: 'preview-3', large: 'large-3' },
+  // ]);
+
+  const images = [
+    { preview: 'preview-1', large: 'large-1' },
+    { preview: 'preview-2', large: 'large-2' },
+    { preview: 'preview-3', large: 'large-3' },
+  ];
+
+  const [selectImageUrl, setSelectImageUrl] = useState(null);
+
+  return (
+    <section>
+      <List items={images} onSelect={setSelectImageUrl} />
+      {selectImageUrl && <Modal url={selectImageUrl} />}
+    </section>
+  );
 }

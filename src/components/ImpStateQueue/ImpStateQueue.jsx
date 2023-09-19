b54fe5e9f987324249513c635dabc4601b004a0d
basesnel <basesnel@gmail.com>
2023-09-19 21:10:13 +0300

    Add 2 task prepared to process it

--- src/components/ImpStateQueue/ImpStateQueue.jsx
+++ src/components/ImpStateQueue/ImpStateQueue.jsx
@@ -1,3 +1,32 @@
+function getFinalState(baseState, queue) {
+  let finalState = baseState;
+
+  return finalState;
+}
+
 export default function ImpStateQueue() {
-  return <>ImpStateQueue</>;
+  return (
+    <>
+      <TestCase baseState={0} queue={[1, 1, 1]} expected={1} />
+    </>
+  );
+}
+
+function TestCase({ baseState, queue, expected }) {
+  const actual = getFinalState(baseState, queue);
+
+  return (
+    <>
+      <p>
+        Base state: <b>{baseState}</b>
+      </p>
+      <p>
+        Queue: <b>{queue}</b>
+      </p>
+      <p style={{ color: actual === expected ? 'correct' : 'wrong' }}>
+        Your resultat: <b>{actual}</b> (
+        {actual === expected ? 'correct' : 'wrong'})
+      </p>
+    </>
+  );
 }

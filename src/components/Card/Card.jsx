79298d7e26a522a8427de29c73d90873a4a7a88c
basesnel <basesnel@gmail.com>
2023-09-24 09:54:42 +0300

    Fix title and text sizes on mobile media

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import css from './Card.module.css';
 
 export default function Card({ children }) {
-  return <section className={css.card}>{children}</section>;
+  return <div className={css.card}>{children}</div>;
 }
 
 Card.propTypes = {

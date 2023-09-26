019dc1a211c159578fbc1c6cba826ba077766807
basesnel <basesnel@gmail.com>
2023-09-26 21:14:21 +0300

    Task II: add some style to select

--- src/components/Canvas/Canvas.jsx
+++ src/components/Canvas/Canvas.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react';
 import Background from './Background.jsx';
 import Box from './Box.jsx';
 
+import css from './Canvas.module.css';
+
 const initialPosition = {
   x: 0,
   y: 0,
@@ -26,8 +28,12 @@ export default function Canvas() {
   };
 
   return (
-    <div style={{ position: 'relative', width: '100%', height: '280px' }}>
-      <select value={shape.color} onChange={handleColorChange}>
+    <div style={{ position: 'relative', width: '100%', height: '300px' }}>
+      <select
+        value={shape.color}
+        onChange={handleColorChange}
+        className={css.option}
+      >
         <option value="orange">orange</option>
         <option value="lightpink">lightpink</option>
         <option value="aliceblue">aliceblue</option>

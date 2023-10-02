e1c17013d618f1bde521b25920786c9ce6a2bd8b
basesnel <basesnel@gmail.com>
2023-10-02 15:06:34 +0300

    Fourth array task is finished

--- src/tasks/ArrayTaskFourth/ArrayTaskThird.jsx
+++ src/tasks/ArrayTaskFourth/ArrayTaskThird.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useImmer } from 'use-immer';
+
 import AddTodo from './AddTodo.js';
 import TaskList from './TaskList.js';
 
@@ -9,33 +10,32 @@ const initialTodos = [
 ];
 
 export default function TaskApp() {
-  const [todos, setTodos] = useState(initialTodos);
+  const [todos, updateTodos] = useImmer(initialTodos);
 
   const handleAddTodo = title => {
-    setTodos([
-      ...todos,
-      {
+    updateTodos(draft => {
+      draft.push({
         id: todos.length,
         title: title,
         done: false,
-      },
-    ]);
+      });
+    });
   };
 
   const handleChangeTodo = nextTodo => {
-    setTodos(
-      todos.map(todo => {
-        if (todo.id === nextTodo.id) {
-          return { ...todo, title: nextTodo.title, done: nextTodo.done };
-        } else {
-          return todo;
-        }
-      })
-    );
+    updateTodos(draft => {
+      const updateTodo = draft.find(t => t.id === nextTodo.id);
+      updateTodo.title = nextTodo.title;
+      updateTodo.done = nextTodo.done;
+    });
   };
 
   const handleDeleteTodo = todoId => {
-    setTodos(todos.filter(todo => todo.id !== todoId));
+    updateTodos(draft => draft.filter(t => t.id !== todoId));
+    // updateTodos(draft => {
+    //   const index = draft.findIndex(t => t.id === todoId);
+    //   draft.splice(index, 1);
+    // });
   };
 
   return (

621a34cedfb27ab21f38140c9421be70ec0199a3
basesnel <basesnel@gmail.com>
2023-10-12 16:39:50 +0300

    Add some style to li-element

--- src/tasks/arrays/ArrayPractSecond/ArrayPractSecond.jsx
+++ src/tasks/arrays/ArrayPractSecond/ArrayPractSecond.jsx
@@ -6,6 +6,8 @@ import DecoratedIconButton from 'components/DecoratedIconButton';
 
 import { initialArtistsList } from 'constants';
 
+import css from './ArrayPractSecond.module.css';
+
 export default function ArrayPractSecond() {
   const [artists, setArtists] = useState(initialArtistsList);
 
@@ -13,14 +15,16 @@ export default function ArrayPractSecond() {
     <>
       <ul>
         {artists.map(artist => (
-          <li key={artist.id}>
-            {artist.name}{' '}
-            <DecoratedIconButton
-              caption={<RiIndeterminateCircleFill />}
-              onClick={() => {
-                setArtists(artists.filter(a => a.id !== artist.id));
-              }}
-            />
+          <li key={artist.id} className={css.item}>
+            <div className={css['item-flex']}>
+              <span className={css['item-content']}>{artist.name}</span>
+              <DecoratedIconButton
+                caption={<RiIndeterminateCircleFill />}
+                onClick={() => {
+                  setArtists(artists.filter(a => a.id !== artist.id));
+                }}
+              />
+            </div>
           </li>
         ))}
       </ul>

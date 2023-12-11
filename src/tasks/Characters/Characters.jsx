8f5b22b71114531b9d75e28bbb112d3c6dea27e5
basesnel <basesnel@gmail.com>
2023-12-11 18:12:30 +0200

    Added independancy of case on filter in characters-task.

--- src/tasks/Characters/Characters.jsx
+++ src/tasks/Characters/Characters.jsx
@@ -2,15 +2,15 @@ import { useState, useMemo } from 'react';
 
 import { FiFilter } from 'react-icons/fi';
 
-import { initialCharacters } from 'constants';
 import Box from 'components/Box';
 import DecoratedButton from 'components/DecoratedButton';
 import DecoratedInput from 'components/DecoratedInput';
-
 import List from 'components/List';
 import Item from 'components/Item';
 import ItemFlex from 'components/ItemFlex';
 import ItemText from 'components/ItemText';
+
+import { initialCharacters } from 'constants';
 
 export default function Characters() {
   const [count, setCount] = useState(0);
@@ -20,7 +20,7 @@ export default function Characters() {
   const visibleCharacters = useMemo(() => {
     console.log('characters is filtering ' + Date.now());
     return characters.filter(character =>
-      character.toLowerCase().includes(filter)
+      character.toLowerCase().includes(filter.toLocaleLowerCase())
     );
   }, [filter, characters]);
 

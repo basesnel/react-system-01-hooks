db326082fb0c6400c968ab54af2a5b68895a1a1a
basesnel <basesnel@gmail.com>
2024-05-17 17:07:54 +0300

    Reformed some components styles and changed prac09 styles of control elements.

--- src/tasks/useEffectsCases/Prac09/Prac09.jsx
+++ src/tasks/useEffectsCases/Prac09/Prac09.jsx
@@ -12,29 +12,31 @@ import Map from './Map';
 import css from './Prac09.module.css';
 
 const Prac09 = () => {
-  const { wrapper } = css;
+  const { wrapControls, wrapper } = css;
 
   const [zoomLevel, setZoomLevel] = useState(0);
   return (
     <>
-      <div className={wrapper}>
-        <FlexBox>
-          <Span>Zuum level: {zoomLevel}x</Span>
-          <IconButton
-            icon={<RiZoomInFill />}
-            caption="add"
-            onClick={() => {
-              setZoomLevel(zoomLevel + 1);
-            }}
-          />
-          <IconButton
-            icon={<RiZoomOutFill />}
-            caption="subtract"
-            onClick={() => {
-              setZoomLevel(zoomLevel - 1);
-            }}
-          />
-        </FlexBox>
+      <div className={wrapControls}>
+        <div className={wrapper}>
+          <FlexBox>
+            <Span>Zuum level: {zoomLevel}x</Span>
+            <IconButton
+              icon={<RiZoomInFill />}
+              caption="add"
+              onClick={() => {
+                setZoomLevel(zoomLevel + 1);
+              }}
+            />
+            <IconButton
+              icon={<RiZoomOutFill />}
+              caption="subtract"
+              onClick={() => {
+                setZoomLevel(zoomLevel - 1);
+              }}
+            />
+          </FlexBox>
+        </div>
       </div>
       <Map zoomLevel={zoomLevel} />
     </>

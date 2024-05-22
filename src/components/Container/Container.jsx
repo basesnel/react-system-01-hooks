4eff6779909841024e8ae8a00d3d346319e15c44
basesnel <basesnel@gmail.com>
2024-05-22 11:24:04 +0300

    Css optimization in Container - Input components.

--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -5,6 +5,7 @@ import css from './Container.module.css';
 const Container = props => {
   const { variant, children } = props;
   const { container } = css;
+
   const variation = variant ? `${css[variant]}` : '';
 
   return (

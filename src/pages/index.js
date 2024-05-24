5dd88dedaf0e32b5f1c2b9b6b00df1ecaa2dfc86
basesnel <basesnel@gmail.com>
2024-05-24 09:42:19 +0300

    Added index to pages for export optimization.

---
+++ src/pages/index.js
@@ -0,0 +1,37 @@
+import { ArrayInStatePage } from './ArrayInStatePage';
+import { BigCardsPage } from './BigCardsPage';
+import { CharactersPage } from './CharactersPage';
+import { ClockPage } from './ClockPage';
+import { ColorPickerPage } from './ColorPickerPage';
+import { EscapeHatchesPageP1 } from './EscapeHatchesPageP1';
+import { EscapeHatchesPageP2 } from './EscapeHatchesPageP2';
+import { FormPage } from './FormPage';
+import { HomePage } from './HomePage';
+import { NewsPage } from './NewsPage';
+import { NotFoundPage } from './NotFoundPage';
+import { ObjectInStatePage } from './ObjectInStatePage';
+import { PokemonPage } from './PokemonPage';
+import { QueueingPage } from './QueueingPage';
+import { SimpleStatesPage } from './SimpleStatesPage';
+import { SkipFirstRenderPage } from './SkipFirstRenderPage';
+import { UseEffectsPage } from './UseEffectsPage';
+
+export {
+  ArrayInStatePage,
+  BigCardsPage,
+  CharactersPage,
+  ClockPage,
+  ColorPickerPage,
+  EscapeHatchesPageP1,
+  EscapeHatchesPageP2,
+  FormPage,
+  HomePage,
+  NewsPage,
+  NotFoundPage,
+  ObjectInStatePage,
+  PokemonPage,
+  QueueingPage,
+  SimpleStatesPage,
+  SkipFirstRenderPage,
+  UseEffectsPage,
+};

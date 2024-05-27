6703fe08865e19cf9383cc44bb199e43d4e40270
basesnel <basesnel@gmail.com>
2024-05-27 10:30:18 +0300

    Added indexes for components.

---
+++ src/components/index.js
@@ -0,0 +1,79 @@
+import AppBar from './AppBar';
+import Box from './Box';
+import Button from './Button';
+import Canvas from './Canvas';
+import Caption from './Caption';
+import Card from './Card';
+import CheckBox from './CheckBox';
+import Container from './Container';
+import CustomLoader from './CustomLoader';
+import FlexBox from './FlexBox';
+import Form from './Form';
+import Grid from './Grid';
+import HideOverflow from './HideOverflow';
+import IconButton from './IconButton';
+import Image from './Image';
+import Input from './Input';
+import Item from './Item';
+import ItemCheckedText from './ItemCheckedText';
+import ItemFlex from './ItemFlex';
+import ItemLabel from './ItemLabel';
+import ItemText from './ItemText';
+import List from './List';
+import Loader from './Loader';
+import Logo from './Logo';
+import MiniForm from './MiniForm';
+import Modal from './Modal';
+import NavBar from './NavBar';
+import Paragraph from './Paragraph';
+import ResponsiveImage from './ResponsiveImage';
+import SearchBar from './SearchBar';
+import Select from './Select';
+import Slide from './Slide';
+import Slider from './Slider';
+import Span from './Span';
+import TextArea from './TextArea';
+import Title from './Title';
+import UserMenu from './UserMenu';
+import VisuallyHidden from './VisuallyHidden';
+
+export {
+  AppBar,
+  Box,
+  Button,
+  Canvas,
+  Caption,
+  Card,
+  CheckBox,
+  Container,
+  CustomLoader,
+  FlexBox,
+  Form,
+  Grid,
+  HideOverflow,
+  IconButton,
+  Image,
+  Input,
+  Item,
+  ItemCheckedText,
+  ItemFlex,
+  ItemLabel,
+  ItemText,
+  List,
+  Loader,
+  Logo,
+  MiniForm,
+  Modal,
+  NavBar,
+  Paragraph,
+  ResponsiveImage,
+  SearchBar,
+  Select,
+  Slide,
+  Slider,
+  Span,
+  TextArea,
+  Title,
+  UserMenu,
+  VisuallyHidden,
+};

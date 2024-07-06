7ac28a933c235123715eb9b7402f8cf4b97587c7
basesnel <basesnel@gmail.com>
2024-07-06 16:05:47 +0300

    Fixed Task06 in EscapeHatchesP02.

--- src/tasks/EscapeHatchesP02/Task06/Task06.jsx
+++ src/tasks/EscapeHatchesP02/Task06/Task06.jsx
@@ -1,34 +1,43 @@
 import { useState, useEffect } from 'react';
-import { CheckBox } from 'components';
+import { Canvas, FlexBox, CheckBox } from 'components';
 
 const Task06 = () => {
   const [position, setPosition] = useState({ x: 0, y: 0 });
   const [canMove, setCanMove] = useState(true);
 
   useEffect(() => {
+    const canvas = document.querySelector('[data-box]');
+
     const handleMove = e => {
-      setPosition({ x: e.clientX, y: e.clientY });
+      setPosition({
+        x: e.clientX - canvas.getBoundingClientRect().x,
+        y: e.clientY - canvas.getBoundingClientRect().y,
+      });
     };
-    window.addEventListener('pointerMove', handleMove);
-    return () => window.removeEventListener('pointerMove', handleMove);
+
+    canvas.addEventListener('pointermove', handleMove);
+
+    return () => canvas.removeEventListener('pointermove', handleMove);
   }, []);
 
   return (
-    <>
-      <CheckBox
-        name="canMove06"
-        checked={canMove}
-        onChange={e => setCanMove(e.target.checked)}
-        label="The dot is allowed to move"
-      />
+    <Canvas data-box>
+      <FlexBox>
+        <CheckBox
+          name="canMove06"
+          checked={canMove}
+          onChange={e => setCanMove(e.target.checked)}
+          label="The dot is allowed to move"
+        />
+      </FlexBox>
       <hr />
       <div
         style={{
           position: 'absolute',
-          BackgroundColor: 'pink',
+          backgroundColor: 'pink',
           borderRadius: '50%',
           opacity: 0.6,
-          transform: `translite(${position.x}px, ${position.y}px)`,
+          transform: `translate(${position.x}px, ${position.y}px)`,
           pointerEvents: 'none',
           left: -20,
           top: -20,
@@ -36,7 +45,7 @@ const Task06 = () => {
           height: 40,
         }}
       />
-    </>
+    </Canvas>
   );
 };
 

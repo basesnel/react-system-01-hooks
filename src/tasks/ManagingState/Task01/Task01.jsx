cac2c350689d8e6592958fb0d1afc4a9687b3d0a
basesnel <basesnel@gmail.com>
2025-05-16 22:27:04 +0300

    Task03 is done.

--- src/tasks/ManagingState/Task01/Task01.jsx
+++ src/tasks/ManagingState/Task01/Task01.jsx
@@ -1,14 +1,24 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 // import { MiniForm, List, Item, ItemFlex, ItemText } from 'components';
 // import { RiAddCircleFill } from 'react-icons/ri';
 
 const Task01 = () => {
+  const [isActive, setIsActive] = useState(false);
+
   return (
-    <div className="background background--active">
+    <div
+      className={!isActive ? 'background background--active' : 'background'}
+      onClick={e => {
+        if (e.target === e.currentTarget) setIsActive(false);
+      }}
+    >
       <img
-        className="picture"
+        className={isActive ? 'picture picture--active' : 'picture'}
         alt="Rainbow houses in Kampung Pelengi, Indonesia"
         src="https://i.imgur.com/5qwVYb1.jpg"
+        onClick={e => {
+          if (e.target === e.currentTarget) setIsActive(true);
+        }}
       />
     </div>
   );

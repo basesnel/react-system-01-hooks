ef7f4a1c0e82126f6293eed8d580440e7fa8242b
basesnel <basesnel@gmail.com>
2025-05-22 16:05:19 +0300

    Settled Task02 in the ManageStatePage00.

--- src/tasks/ManagingState/Task02/Task02.jsx
+++ src/tasks/ManagingState/Task02/Task02.jsx
@@ -1,21 +1,48 @@
-// import { useState } from 'react';
+import { useState } from 'react';
 
 // import styles from './styles.module.css';
 
 const Task02 = () => {
+  const [firstName, setFirstName] = useState('Jane');
+  const [lastName, setLastName] = useState('Jacobs');
+  const [isProfileEditing, setIsProfileEditing] = useState(false);
+
+  const handleFirstNameChange = e => {
+    setFirstName(e.target.value);
+  };
+  const handleLastNameChange = e => {
+    setLastName(e.target.value);
+  };
+
   return (
     <form>
       <label>
-        First name: <b>Jane</b>
-        <input />
+        First name:
+        {!isProfileEditing ? (
+          <b>{firstName}</b>
+        ) : (
+          <input value={firstName} onChange={handleFirstNameChange} />
+        )}
       </label>
       <label>
-        Last name: <b>Jacobs</b>
-        <input />
+        Last name:{' '}
+        {!isProfileEditing ? (
+          <b>{lastName}</b>
+        ) : (
+          <input value={lastName} onChange={handleLastNameChange} />
+        )}
       </label>
-      <button type="submit">Edit profile</button>
+      <button
+        type="submit"
+        onClick={e => {
+          e.preventDefault();
+          setIsProfileEditing(!isProfileEditing);
+        }}
+      >
+        {!isProfileEditing ? 'Edit profile' : 'Save profile'}
+      </button>
       <p>
-        <i>Hello, Jane Jacobs!</i>
+        <i>{`Hello, ${firstName} ${lastName}!`}</i>
       </p>
     </form>
   );

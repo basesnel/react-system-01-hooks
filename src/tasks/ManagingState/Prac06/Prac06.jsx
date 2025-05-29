d5ecdc90a6e377ec52832498af49c72e17c0e1a4
basesnel <basesnel@gmail.com>
2025-05-29 19:59:07 +0300

    Added Prac06 into the ManagingStatePage.

--- src/tasks/ManagingState/Prac06/Prac06.jsx
+++ src/tasks/ManagingState/Prac06/Prac06.jsx
@@ -1,78 +1,50 @@
 import { useState } from 'react';
 import { FiEdit3 } from 'react-icons/fi';
-import { Button, FlexBox, Form, Paragraph, TextArea, Title } from 'components';
+import { Button, FlexBox, Form, Paragraph, TextArea } from 'components';
 
 const Prac06 = () => {
-  const [answer, setAnswer] = useState('');
-  const [error, setError] = useState(null);
-  const [status, setStatus] = useState('typing');
-
-  if (status === 'success')
-    return (
-      <Paragraph centered succeeded>
-        That's right!
-      </Paragraph>
-    );
+  const [text, setText] = useState('');
+  const [isSending, setIsSending] = useState(false);
+  const [isSent, setIsSent] = useState(false);
 
   async function handleSubmit(e) {
     e.preventDefault();
-    setStatus('submitting');
-    try {
-      await submitForm(answer);
-      setStatus('success');
-    } catch (err) {
-      setStatus('typing');
-      setError(err);
-    }
+    setIsSending(true);
+    await sendMessage(text);
+    setIsSending(false);
+    setIsSent(true);
   }
 
-  function handleTextareaChange(e) {
-    setAnswer(e.target.value);
-  }
+  if (isSent)
+    return (
+      <Paragraph centered succeeded>
+        Thanks for feedback!
+      </Paragraph>
+    );
 
   return (
     <>
-      <Title level={3} caption="city quiz" />
-      <Paragraph>
-        In which city is there a billboard that runs air into drinkable water
-      </Paragraph>
       <Form onSubmit={handleSubmit}>
         <TextArea
-          isDisabled={status === 'submitting'}
-          textAreaName="prac01textarea"
-          textAreaValue={answer}
-          textAreaLabel="Your answer:"
-          textAreaPlaceholder="Type your answer..."
-          handleChange={handleTextareaChange}
+          isDisabled={isSending}
+          textAreaName="textareaP06"
+          textAreaValue={text}
+          textAreaLabel="Your feedback:"
+          textAreaPlaceholder="Type your feedback..."
+          handleChange={e => setText(e.target.value)}
           icon={<FiEdit3 />}
         />
         <FlexBox>
-          <Button
-            type="submit"
-            isDisabled={status === 'empty' || status === 'submitting'}
-            caption="Submit"
-          />
+          <Button type="submit" isDisabled={isSending} caption="Send" />
         </FlexBox>
-        {error !== null && (
-          <Paragraph warned centered>
-            {error.message}
-          </Paragraph>
-        )}
       </Form>
     </>
   );
 };
 
-function submitForm(answer) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      let shouldError = answer.toLowerCase() !== 'lima';
-      if (shouldError) {
-        reject(new Error('Good guess but a wrong answer. Try again!'));
-      } else {
-        resolve();
-      }
-    }, 1500);
+function sendMessage(text) {
+  return new Promise(resolve => {
+    setTimeout(resolve, 2000);
   });
 }
 

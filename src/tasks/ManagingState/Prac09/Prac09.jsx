82129de2cbdcccf0b3349060227fb518249870c6
basesnel <basesnel@gmail.com>
2025-06-05 14:53:55 +0300

    Reformed prac09 interactively - ManagingStatePage page.

--- src/tasks/ManagingState/Prac09/Prac09.jsx
+++ src/tasks/ManagingState/Prac09/Prac09.jsx
@@ -4,17 +4,30 @@ import { Title } from 'components';
 
 import styles from './styles.module.css';
 
-const PlaceTree = ({ id, placesById }) => {
+const PlaceTree = ({ id, parentId, placesById, onComplete }) => {
   const place = placesById[id];
   const childIds = place.childIds;
 
   return (
     <li className={styles.item}>
       {place.title}
+      <button
+        onClick={() => {
+          onComplete(parentId, id);
+        }}
+      >
+        Complete
+      </button>
       {childIds.length > 0 && (
         <ol className={styles.list}>
           {childIds.map(childId => (
-            <PlaceTree key={childId} id={childId} placesById={placesById} />
+            <PlaceTree
+              key={childId}
+              id={childId}
+              parentId={id}
+              placesById={placesById}
+              onComplete={onComplete}
+            />
           ))}
         </ol>
       )}
@@ -23,16 +36,36 @@ const PlaceTree = ({ id, placesById }) => {
 };
 
 const Prac09 = () => {
-  const [plan] = useState(modifiedTravelPlan);
+  const [plan, setPlan] = useState(modifiedTravelPlan);
   const root = plan[0];
   const planets = root.childIds;
+
+  const handleComplete = (parentId, childId) => {
+    const parent = plan[parentId];
+
+    const nextParent = {
+      ...parent,
+      childIds: parent.childIds.filter(id => id !== childId),
+    };
+
+    setPlan({
+      ...plan,
+      [parentId]: nextParent,
+    });
+  };
 
   return (
     <>
       <Title level={3} caption="places to visit" />
       <ol className={styles.list}>
         {planets.map(id => (
-          <PlaceTree key={id} id={id} placesById={plan} />
+          <PlaceTree
+            key={id}
+            id={id}
+            parentId={0}
+            placesById={plan}
+            onComplete={handleComplete}
+          />
         ))}
       </ol>
     </>

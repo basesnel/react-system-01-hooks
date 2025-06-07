c633812226b43292ac8733061ca589efe44fb0ff
basesnel <basesnel@gmail.com>
2025-06-07 12:45:43 +0300

    Added PlaceTree component to Prac08 on ManagingState page.

---
+++ src/tasks/ManagingState/Prac08/PlaceTree.jsx
@@ -0,0 +1,57 @@
+import PropTypes from 'prop-types';
+import { NumberedList, Item, ItemFlex } from 'components';
+
+const PlaceTree = ({ place }) => {
+  const childPlaces = place.childPlaces;
+
+  return (
+    <Item>
+      <ItemFlex>{place.title}</ItemFlex>
+      {childPlaces.length > 0 && (
+        <NumberedList message="no items in the plan">
+          {childPlaces.map(place => (
+            <PlaceTree key={place.id} place={place} />
+          ))}
+        </NumberedList>
+      )}
+    </Item>
+  );
+};
+
+PlaceTree.propTypes = {
+  place: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    title: PropTypes.string.isRequired,
+    childPlaces: PropTypes.arrayOf(PropTypes.object.isRequired),
+  }).isRequired,
+};
+
+PlaceTree.defaultProps = {
+  place: { childPlaces: null },
+};
+
+// PlaceTree.propTypes = {
+//   place: PropTypes.shape({
+//     id: PropTypes.number.isRequired,
+//     title: PropTypes.string.isRequired,
+//     childPlaces: PropTypes.oneOf([
+//       null,
+//       PropTypes.arrayOf(
+//         PropTypes.shape({
+//           id: PropTypes.number.isRequired,
+//           title: PropTypes.string.isRequired,
+//           childPlaces: PropTypes.oneOf([
+//             null,
+//             PropTypes.arrayOf(PropTypes.object.isRequired),
+//           ]),
+//         }).isRequired
+//       ),
+//     ]),
+//   }).isRequired,
+// };
+
+// PlaceTree.defaultProps = {
+//   place: { childPlaces: null },
+// };
+
+export default PlaceTree;
--- src/tasks/ManagingState/Prac08/Prac08.jsx
+++ src/tasks/ManagingState/Prac08/Prac08.jsx
@@ -1,24 +1,7 @@
 import { useState } from 'react';
-import PropTypes from 'prop-types';
-import { Title, NumberedList, Item, ItemFlex } from 'components';
+import { Title, NumberedList } from 'components';
 import { initialTravelPlan } from 'constants';
-
-const PlaceTree = ({ place }) => {
-  const childPlaces = place.childPlaces;
-
-  return (
-    <Item>
-      <ItemFlex>{place.title}</ItemFlex>
-      {childPlaces.length > 0 && (
-        <NumberedList message="no items in the plan">
-          {childPlaces.map(place => (
-            <PlaceTree key={place.id} place={place} />
-          ))}
-        </NumberedList>
-      )}
-    </Item>
-  );
-};
+import PlaceTree from './PlaceTree';
 
 const Prac08 = () => {
   const [plan] = useState(initialTravelPlan);
@@ -34,30 +17,6 @@ const Prac08 = () => {
       </NumberedList>
     </>
   );
-};
-
-PlaceTree.propTypes = {
-  place: PropTypes.shape({
-    id: PropTypes.number.isRequired,
-    title: PropTypes.string.isRequired,
-    childPlaces: PropTypes.oneOf([
-      null,
-      PropTypes.arrayOf(
-        PropTypes.shape({
-          id: PropTypes.number.isRequired,
-          title: PropTypes.string.isRequired,
-          childPlaces: PropTypes.oneOf([
-            null,
-            PropTypes.arrayOf(PropTypes.object.isRequired),
-          ]),
-        }).isRequired
-      ),
-    ]),
-  }).isRequired,
-};
-
-PlaceTree.defaultProps = {
-  place: { childPlaces: null },
 };
 
 export default Prac08;

b8838450e38e0bae7821c4a3e01d6f0f10d9dabe
basesnel <basesnel@gmail.com>
2025-06-09 19:58:15 +0300

    Settled Task04 on ManagingState page.

--- src/tasks/ManagingState/Task04/Clock.jsx
+++ src/tasks/ManagingState/Task04/Clock.jsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 
 const Clock = props => {
-  const [color] = useState(props.color);
-  return <h3 style={{ color: color }}>{props.time}</h3>;
+  const { color, time } = props;
+  // const [color] = useState(props.color);
+  return <h3 style={{ color: color }}>{time}</h3>;
 };
 
 export default Clock;

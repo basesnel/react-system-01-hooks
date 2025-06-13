6d72c765c7436514f4d2fa576562a6897b7fa0f0
basesnel <basesnel@gmail.com>
2025-06-13 20:04:36 +0300

    Added PackingItem to the Task05 of Managing state.

--- src/tasks/ManagingState/Task05/PackingList.jsx
+++ src/tasks/ManagingState/Task05/PackingList.jsx
@@ -1,6 +1,4 @@
-// import { useState } from 'react';
 import { RiDeleteBinFill } from 'react-icons/ri';
-
 import { List, Item, ItemLabel, IconButton, ItemCheckedText } from 'components';
 
 const PackingList = props => {
@@ -9,26 +7,38 @@ const PackingList = props => {
   return (
     <List message="There is no elements in list.">
       {items.map(item => (
-        <Item key={item.id}>
-          <ItemLabel>
-            <input
-              type="checkbox"
-              name={item.id}
-              checked={item.packed}
-              onChange={e => {
-                onChangeItem({ ...item, packed: e.target.checked });
-              }}
-            />
-            <ItemCheckedText content={item.title} checked={item.packed} />
-            <IconButton
-              icon={<RiDeleteBinFill />}
-              caption="delete"
-              onClick={() => onDeleteItem(item.id)}
-            />
-          </ItemLabel>
-        </Item>
+        <PackingItem
+          item={item}
+          onChangeItem={onChangeItem}
+          onDeleteItem={onDeleteItem}
+        />
       ))}
     </List>
+  );
+};
+
+const PackingItem = props => {
+  const { item, onChangeItem, onDeleteItem } = props;
+
+  return (
+    <Item key={item.id}>
+      <ItemLabel>
+        <input
+          type="checkbox"
+          name={item.id}
+          checked={item.packed}
+          onChange={e => {
+            onChangeItem({ ...item, packed: e.target.checked });
+          }}
+        />
+        <ItemCheckedText content={item.title} checked={item.packed} />
+        <IconButton
+          icon={<RiDeleteBinFill />}
+          caption="delete"
+          onClick={() => onDeleteItem(item.id)}
+        />
+      </ItemLabel>
+    </Item>
   );
 };
 

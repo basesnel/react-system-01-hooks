ba39b514baccb1abf9d7c05bea7cd0eaf2ab240e
basesnel <basesnel@gmail.com>
2025-06-14 19:32:32 +0300

    Applied HideOverflow-component at Prac08 on ManagingState.

--- src/tasks/ManagingState/Prac08/Prac08.jsx
+++ src/tasks/ManagingState/Prac08/Prac08.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Title, NumberedList } from 'components';
+import { HideOverflow, Title, NumberedList } from 'components';
 import { initialTravelPlan } from 'constants';
 import PlaceTree from './PlaceTree';
 
@@ -8,14 +8,14 @@ const Prac08 = () => {
   const planets = plan.childPlaces;
 
   return (
-    <>
+    <HideOverflow>
       <Title level={3} caption="places to visit" />
       <NumberedList message="no items in the plan">
         {planets.map(place => (
           <PlaceTree key={place.id} place={place} />
         ))}
       </NumberedList>
-    </>
+    </HideOverflow>
   );
 };
 

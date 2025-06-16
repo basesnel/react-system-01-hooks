0b201760205c555cee7c2550334b8a89d608fc2b
basesnel <basesnel@gmail.com>
2025-06-16 20:39:59 +0300

    Corrected letters-array in the Task08 and Task09 on ManagingState.

--- src/tasks/ManagingState/Task08/Task08.jsx
+++ src/tasks/ManagingState/Task08/Task08.jsx
@@ -5,9 +5,9 @@ import { useState } from 'react';
 import Letter from './Letter';
 
 const initialLetters = [
-  { id: 0, subject: 'Ready for adventure?', isStarred: true },
-  { id: 1, subject: 'Time to check in!', isStarred: false },
-  { id: 2, subject: 'Festival Begins In Just SEVEN Days!', isStarred: false },
+  { id: 0, subject: 'Ready for adventure?' },
+  { id: 1, subject: 'Time to check in!' },
+  { id: 2, subject: 'Festival Begins In Just SEVEN Days!' },
 ];
 
 const Task08 = () => {

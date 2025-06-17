8810605e25b704487918c15770616409d11dc6d0
basesnel <basesnel@gmail.com>
2025-06-17 19:09:33 +0300

    Import initialLettersStarred to Task06 & Task07 from constants.

--- src/tasks/ManagingState/Task07/Task07.jsx
+++ src/tasks/ManagingState/Task07/Task07.jsx
@@ -1,17 +1,18 @@
 import { useState } from 'react';
+import { initialLettersStarred } from 'constants';
 // import { RiAddCircleFill } from 'react-icons/ri';
 // import { Caption, MiniForm } from 'components';
 // import PackingList from './PackingList';
 import Letter from './Letter';
 
-const initialLetters = [
-  { id: 0, subject: 'Ready for adventure?', isStarred: true },
-  { id: 1, subject: 'Time to check in!', isStarred: false },
-  { id: 2, subject: 'Festival Begins In Just SEVEN Days!', isStarred: false },
-];
+// const initialLetters = [
+//   { id: 0, subject: 'Ready for adventure?', isStarred: true },
+//   { id: 1, subject: 'Time to check in!', isStarred: false },
+//   { id: 2, subject: 'Festival Begins In Just SEVEN Days!', isStarred: false },
+// ];
 
 const Task07 = () => {
-  const [letters, setLetters] = useState(initialLetters);
+  const [letters, setLetters] = useState(initialLettersStarred);
 
   const selectedCount = letters.reduce((acc, item) => acc + item.isStarred, 0);
 

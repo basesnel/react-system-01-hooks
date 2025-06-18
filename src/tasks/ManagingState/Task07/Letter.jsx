744a48826defe6f4b4f6e60cdef2fa7709089fec
basesnel <basesnel@gmail.com>
2025-06-18 19:09:53 +0300

    Added prop checking for Letter of Task07 on ManagingStatePage.

--- src/tasks/ManagingState/Task07/Letter.jsx
+++ src/tasks/ManagingState/Task07/Letter.jsx
@@ -1,8 +1,6 @@
-// import PropTypes from 'prop-types';
-// import { RiDeleteBinFill } from 'react-icons/ri';
-// import { List, Item, ItemLabel, IconButton, ItemCheckedText } from 'components';
-
+import PropTypes from 'prop-types';
 import { ItemCheckedText } from 'components';
+
 import styles from './styles.module.css';
 
 const Letter = props => {
@@ -28,26 +26,14 @@ const Letter = props => {
   );
 };
 
-// PackingList.propTypes = {
-//   items: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-//       title: PropTypes.string.isRequired,
-//       packed: PropTypes.bool.isRequired,
-//     }).isRequired
-//   ).isRequired,
-//   onChangeItem: PropTypes.func.isRequired,
-//   onDeleteItem: PropTypes.func.isRequired,
-// };
-
-// PackingItem.propTypes = {
-//   item: PropTypes.shape({
-//     id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-//     title: PropTypes.string.isRequired,
-//     packed: PropTypes.bool.isRequired,
-//   }).isRequired,
-//   onChangeItem: PropTypes.func.isRequired,
-//   onDeleteItem: PropTypes.func.isRequired,
-// };
+Letter.propTypes = {
+  letter: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    subject: PropTypes.string.isRequired,
+    isStarred: PropTypes.bool.isRequired,
+  }).isRequired,
+  onToggle: PropTypes.func.isRequired,
+  isSelected: PropTypes.bool.isRequired,
+};
 
 export default Letter;

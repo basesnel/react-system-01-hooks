a1bfb579f73335e259a64a8cc16e3889b3bf3fc4
basesnel <basesnel@gmail.com>
2025-11-11 15:59:06 +0200

    ContactList in the task05 of managingState page II is styled.

--- src/tasks/ManagingStateP02/Task05/ContactList.jsx
+++ src/tasks/ManagingStateP02/Task05/ContactList.jsx
@@ -1,37 +1,38 @@
-// import PropTypes from 'prop-types';
-// import { Button } from 'components';
+import PropTypes from 'prop-types';
+import { Button } from 'components';
 
-// import styles from './styles.module.css';
+import styles from './styles.module.css';
 
 const ContactList = ({ contacts, selectedId, onSelect }) => {
   return (
-    <section>
-      <ul>
-        {contacts.map(contact => (
-          <li key={contact.id}>
-            <button
-              onClick={() => {
-                onSelect(contact.id);
-              }}
-            >
-              {contact.id === selectedId ? <b>{contact.name}</b> : contact.name}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </section>
+    <ul className={styles.list}>
+      {contacts.map(contact => (
+        <li key={contact.id} className={styles.item}>
+          <Button
+            caption={
+              contact.id === selectedId ? <b>{contact.name}</b> : contact.name
+            }
+            full
+            onClick={() => {
+              onSelect(contact.id);
+            }}
+          />
+        </li>
+      ))}
+    </ul>
   );
 };
 
-// ContactList.propTypes = {
-//   contacts: PropTypes.arrayOf(
-//     PropTypes.shape({
-//       id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
-//       name: PropTypes.string.isRequired,
-//       email: PropTypes.string.isRequired,
-//     }).isRequired
-//   ).isRequired,
-//   onSelect: PropTypes.func.isRequired,
-// };
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      name: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  selectedId: PropTypes.number.isRequired,
+  onSelect: PropTypes.func.isRequired,
+};
 
 export default ContactList;

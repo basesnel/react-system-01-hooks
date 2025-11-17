8fec9addc58eca13c3ec41ab82f4a30e988ea4f2
basesnel <basesnel@gmail.com>
2025-11-17 15:54:16 +0200

    TaskList component - prac01 is styled in the managing-state page III

--- src/tasks/ManagingStateP03/Prac01/TaskList.jsx
+++ src/tasks/ManagingStateP03/Prac01/TaskList.jsx
@@ -1,14 +1,25 @@
 import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { RiSave3Fill, RiFileEditFill, RiDeleteBinFill } from 'react-icons/ri';
+import { FiEdit3 } from 'react-icons/fi';
+import {
+  List,
+  Item,
+  ItemLabel,
+  IconButton,
+  Input,
+  ItemCheckedText,
+} from 'components';
 
 const TaskList = ({ tasks, onChangeTask, onDeleteTask }) => {
   return (
-    <ul>
+    <List message="There is no elements in list.">
       {tasks.map(task => (
-        <li key={task.id}>
+        <Item key={task.id}>
           <Task task={task} onChange={onChangeTask} onDelete={onDeleteTask} />
-        </li>
+        </Item>
       ))}
-    </ul>
+    </List>
   );
 };
 
@@ -19,25 +30,45 @@ const Task = ({ task, onChange, onDelete }) => {
   if (isEditing) {
     taskContent = (
       <>
-        <input
+        {/* <input
           value={task.text}
           onChange={e => {
             onChange({ ...task, text: e.target.value });
           }}
+        /> */}
+        <Input
+          inputName={`TaskP01 ${task.id}`}
+          inputValue={task.text}
+          handleChange={e => {
+            onChange({ ...task, text: e.target.value });
+          }}
+          icon={<FiEdit3 />}
         />
-        <button onClick={() => setIsEditing(false)}>Save</button>
+        {/* <button onClick={() => setIsEditing(false)}>Save</button> */}
+        <IconButton
+          icon={<RiSave3Fill />}
+          caption="save"
+          onClick={() => setIsEditing(false)}
+        />
       </>
     );
   } else {
     taskContent = (
       <>
-        {task.text}
-        <button onClick={() => setIsEditing(true)}>Edit</button>
+        <ItemCheckedText content={task.text} checked={task.done} />
+        {/* <button onClick={() => setIsEditing(true)}>Edit</button> */}
+        <IconButton
+          icon={<RiFileEditFill />}
+          caption="edit"
+          onClick={() => setIsEditing(true)}
+        />
       </>
     );
   }
+
   return (
-    <label>
+    // <label>
+    <ItemLabel>
       <input
         type="checkbox"
         checked={task.done}
@@ -46,9 +77,36 @@ const Task = ({ task, onChange, onDelete }) => {
         }}
       />
       {taskContent}
-      <button onClick={() => onDelete(task.id)}>Delete</button>
-    </label>
+      {/* <button onClick={() => onDelete(task.id)}>Delete</button> */}
+      <IconButton
+        icon={<RiDeleteBinFill />}
+        caption="delete"
+        onClick={() => onDelete(task.id)}
+      />
+    </ItemLabel>
   );
+};
+
+TaskList.propTypes = {
+  tasks: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      text: PropTypes.string.isRequired,
+      done: PropTypes.bool.isRequired,
+    }).isRequired
+  ).isRequired,
+  onChangeTask: PropTypes.func.isRequired,
+  onDeleteTask: PropTypes.func.isRequired,
+};
+
+Task.propTypes = {
+  task: PropTypes.shape({
+    id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+    text: PropTypes.string.isRequired,
+    done: PropTypes.bool.isRequired,
+  }).isRequired,
+  onChange: PropTypes.func.isRequired,
+  onDelete: PropTypes.func.isRequired,
 };
 
 export default TaskList;

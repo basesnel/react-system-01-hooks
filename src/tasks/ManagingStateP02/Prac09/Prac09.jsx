8b7469471b90cc267717cf53b326ae58e9c78514
basesnel <basesnel@gmail.com>
2025-11-19 15:47:13 +0200

    Counter is extracted from prac09 in the managing-state-page II

--- src/tasks/ManagingStateP02/Prac09/Prac09.jsx
+++ src/tasks/ManagingStateP02/Prac09/Prac09.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import PropTypes from 'prop-types';
-import { Button, CheckBox, FlexBox, Span } from 'components';
+import { CheckBox, FlexBox } from 'components';
+import { CounterP06 } from '../intendendComponents';
 
 import styles from './styles.module.css';
 
@@ -11,11 +11,11 @@ const Prac09 = () => {
     <>
       {isFancy ? (
         <FlexBox>
-          <Counter isFancy={true} />
+          <CounterP06 isFancy={true} />
         </FlexBox>
       ) : (
         <section className={styles.flexed}>
-          <Counter isFancy={false} />
+          <CounterP06 isFancy={false} />
         </section>
       )}
       <CheckBox
@@ -26,33 +26,6 @@ const Prac09 = () => {
       />
     </>
   );
-};
-
-const Counter = ({ isFancy }) => {
-  const [score, setScore] = useState(0);
-  const [hover, setHover] = useState(false);
-
-  let className = `${styles.counter}`;
-  let color = isFancy ? '#ff6767' : null;
-  if (hover) className += ` ${styles.hover}`;
-  if (isFancy) className += ` ${styles.fancy}`;
-
-  return (
-    <div
-      className={className}
-      onPointerEnter={() => setHover(true)}
-      onPointerLeave={() => setHover(false)}
-    >
-      <Span weighted color={color}>
-        {score}
-      </Span>
-      <Button caption="Add one" onClick={() => setScore(score + 1)} />
-    </div>
-  );
-};
-
-Counter.propTypes = {
-  isFancy: PropTypes.bool.isRequired,
 };
 
 export default Prac09;

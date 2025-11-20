18a2de891d20273a5716484a931364c26fd81ca4
basesnel <basesnel@gmail.com>
2025-11-20 16:10:06 +0200

    Checking lint constraints.

--- src/tasks/ManagingStateP03/Task04/myReact.js
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react';
-
-const useReducer = (reducer, initialState) => {
-  const [state, setState] = useState(initialState);
-
-  return [state, dispatch];
-};
-
-export default useReducer;

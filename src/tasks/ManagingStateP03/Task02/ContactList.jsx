ab3792d8ac6900aaa583d055ca53d35f5a0208fd
basesnel <basesnel@gmail.com>
2025-11-25 14:49:56 +0200

    ContactList of task02 is styled in the managing-state page III.

--- src/tasks/ManagingStateP03/Task02/ContactList.jsx
+++ src/tasks/ManagingStateP03/Task02/ContactList.jsx
@@ -1,24 +1,41 @@
+import PropTypes from 'prop-types';
+import { Button } from 'components';
+
+import styles from './styles.module.css';
+
 const ContactList = ({ contacts, selectedId, dispatch }) => {
   return (
-    <section>
-      <ul>
-        {contacts.map(contact => (
-          <li key={contact.id}>
-            <button
-              onClick={() => {
-                dispatch({
-                  type: 'changed_selection',
-                  contactId: contact.id,
-                });
-              }}
-            >
-              {selectedId === contact.id ? <b>{contact.name}</b> : contact.name}
-            </button>
-          </li>
-        ))}
-      </ul>
-    </section>
+    <ul className={styles.list}>
+      {contacts.map(contact => (
+        <li key={contact.id} className={styles.item}>
+          <Button
+            caption={
+              contact.id === selectedId ? <b>{contact.name}</b> : contact.name
+            }
+            full
+            onClick={() => {
+              dispatch({
+                type: 'changed_selection',
+                contactId: contact.id,
+              });
+            }}
+          />
+        </li>
+      ))}
+    </ul>
   );
+};
+
+ContactList.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+      name: PropTypes.string.isRequired,
+      email: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  selectedId: PropTypes.number.isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 
 export default ContactList;

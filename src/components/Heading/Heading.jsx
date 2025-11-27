55fdfec5254d27b32f93c15a7b4af95c6b1ee07c
basesnel <basesnel@gmail.com>
2025-11-27 21:20:29 +0200

    Heading component is styled.

--- src/components/Heading/Heading.jsx
+++ src/components/Heading/Heading.jsx
@@ -10,22 +10,34 @@ const Heading = props => {
       throw Error('Heading must be inside a section!');
 
     case 1:
-      return <h1 className={styles.title01}>{children}</h1>;
+      return (
+        <h1 className={`${styles.heading} ${styles.heading01}`}>{children}</h1>
+      );
 
     case 2:
-      return <h2 className={styles.title02}>{children}</h2>;
+      return (
+        <h2 className={`${styles.heading} ${styles.heading02}`}>{children}</h2>
+      );
 
     case 3:
-      return <h3 className={styles.title03}>{children}</h3>;
+      return (
+        <h3 className={`${styles.heading} ${styles.heading03}`}>{children}</h3>
+      );
 
     case 4:
-      return <h4 className={styles.title04}>{children}</h4>;
+      return (
+        <h4 className={`${styles.heading} ${styles.heading04}`}>{children}</h4>
+      );
 
     case 5:
-      return <h5 className={styles.title05}>{children}</h5>;
+      return (
+        <h5 className={`${styles.heading} ${styles.heading05}`}>{children}</h5>
+      );
 
     case 6:
-      return <h6 className={styles.title06}>{children}</h6>;
+      return (
+        <h6 className={`${styles.heading} ${styles.heading06}`}>{children}</h6>
+      );
 
     default:
       throw Error('Unknown level: ' + level);

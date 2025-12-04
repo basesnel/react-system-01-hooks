7a3c3c362415bf0761a2cb21c1cf7b3e138b884f
basesnel <basesnel@gmail.com>
2025-12-04 19:38:05 +0200

    Data is extracted from prac04 in the managing-state page III

--- src/tasks/ManagingStateP03/Prac04/Prac04.jsx
+++ src/tasks/ManagingStateP03/Prac04/Prac04.jsx
@@ -1,14 +1,10 @@
 import { useImmerReducer } from 'use-immer';
 import { Title } from 'components';
 import tasksReducer from './tasksReducer';
+import { initialTasks } from 'constants';
 import { AddTaskP01, TaskListP01 } from '../IntendendComponents';
 
 let nextId = 3;
-const initialTasks = [
-  { id: 0, text: 'Visit Kafka Museum', done: true },
-  { id: 1, text: 'Watch a puppet show', done: false },
-  { id: 2, text: 'Lennon Wall pic', done: false },
-];
 
 const Prac04 = () => {
   const [tasks, dispatch] = useImmerReducer(tasksReducer, initialTasks);

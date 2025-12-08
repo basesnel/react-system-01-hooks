631f4e899fe05eccfdd7aaeb94ac664112306577
basesnel <basesnel@gmail.com>
2025-12-08 15:28:34 +0200

    Propses are added to Section - prac05 in the managing-page-iii.

--- src/tasks/ManagingStateP03/IntendendComponents/SectionP05/SectionP05.jsx
+++ src/tasks/ManagingStateP03/IntendendComponents/SectionP05/SectionP05.jsx
@@ -1,7 +1,13 @@
+import PropTypes from 'prop-types';
+
 import styles from './styles.module.css';
 
 const SectionP05 = ({ children }) => {
   return <section className={styles.section}>{children}</section>;
+};
+
+SectionP05.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default SectionP05;

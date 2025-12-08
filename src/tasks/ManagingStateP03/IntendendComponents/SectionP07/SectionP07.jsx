e83076eab00ac7830a03714c05e7d6130a97b630
basesnel <basesnel@gmail.com>
2025-12-08 15:45:06 +0200

    Propses are added to Section - prac07 in the managing-page-iii.

--- src/tasks/ManagingStateP03/IntendendComponents/SectionP07/SectionP07.jsx
+++ src/tasks/ManagingStateP03/IntendendComponents/SectionP07/SectionP07.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import { LevelContext } from 'contexts/mpiiiContexts/LevelContextP07';
 import styles from './styles.module.css';
 
@@ -7,6 +8,11 @@ const SectionP07 = ({ level, children }) => {
       <LevelContext.Provider value={level}>{children}</LevelContext.Provider>
     </section>
   );
+};
+
+SectionP07.propTypes = {
+  level: PropTypes.number.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default SectionP07;

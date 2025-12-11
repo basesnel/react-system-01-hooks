97b8d0985900ade359cc6f22f7a1492319237a76
basesnel <basesnel@gmail.com>
2025-12-11 15:58:23 +0200

    Chat is extracted from task04 in the managing-page-iii.

--- src/tasks/ManagingStateP03/Task04/Task04.jsx
+++ src/tasks/ManagingStateP03/Task04/Task04.jsx
@@ -2,7 +2,7 @@ import { useReducer } from './myReact';
 import { FlexBox } from 'components';
 import { initialState, messengerReducer } from './messengerReducer';
 import ContactList from './ContactList';
-import Chat from './Chat';
+import { ChatT03 } from '../IntendendComponents';
 
 const contacts = [
   { id: 0, name: 'Taylor', email: 'taylor@mail.com' },
@@ -22,7 +22,7 @@ const Task04 = () => {
         selectedId={state.selectedId}
         dispatch={dispatch}
       />
-      <Chat
+      <ChatT03
         key={contact.id}
         message={message}
         contact={contact}

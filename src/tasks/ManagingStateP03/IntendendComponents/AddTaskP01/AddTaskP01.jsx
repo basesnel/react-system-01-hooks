67a23c48efb91e47eaa080e4d3ff1aeeb51d0d7a
basesnel <basesnel@gmail.com>
2025-12-25 14:14:13 +0200

    AddTaskP01 is cleared up in the the managing-state-page-iii.

--- src/tasks/ManagingStateP03/IntendendComponents/AddTaskP01/AddTaskP01.jsx
+++ src/tasks/ManagingStateP03/IntendendComponents/AddTaskP01/AddTaskP01.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { RiAddCircleFill } from 'react-icons/ri';
 import { MiniForm } from 'components';
 
 const AddTaskP01 = ({ onAddTask }) => {
-  const [text, setText] = useState('');
-
   return (
     <MiniForm
       onFormSubmit={text => {
         onAddTask(text);
-        setText('');
       }}
-      filling={`Add element ${text}`}
+      filling="Add element"
       icon={<RiAddCircleFill />}
     />
   );

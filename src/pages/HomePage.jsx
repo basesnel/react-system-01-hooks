805ebfb58381262ca37c3c5c4f09e1662210703d
basesnel <basesnel@gmail.com>
2026-01-18 14:10:27 +0200

    Added some content to home-page for working.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,11 @@ const HomePage = () => {
       <Container>
         <H level={1}>react hook cases</H>
         <H level={2}>Welcome to hook cases on React!</H>
-        <P>This practice project is about experiences of React-hooks.</P>
+        <P>
+          Content of this page isn't important. But I need it to think headings
+          and text over by describing this project.
+        </P>
+        <P>This practice project is about React-hooks' cases.</P>
         <H level={3}>Project's structure</H>
         <P>There is short project's description.</P>
         <H level={4}>Form page</H>
@@ -18,7 +22,7 @@ const HomePage = () => {
           Pokemon page shows simple Rest-request to Pokemon-api case on React.
         </P>
         <H level={4}>Simple-state page</H>
-        <P>Simple page shows simple useState-hook behaviors on React.</P>
+        <P>Simple page shows simple useState-hook cases on React.</P>
         <H level={5}>Practice 01</H>
         <P>
           It shows useState-hook case on React for components with own state.
@@ -28,6 +32,31 @@ const HomePage = () => {
           It shows useState-hook case on React for components with general
           state.
         </P>
+        <H level={5}>Practice 03</H>
+        <P>
+          In this case title of web-page shows sum of buttons' clickings by
+          useEffect.
+        </P>
+        <H level={4}>Manage-state page part II</H>
+        <P>It shows some managing of state cases on React for components.</P>
+        <H level={5}>Practice 01</H>
+        <P>It shows how state lifts up.</P>
+        <H level={6}>About</H>
+        <P>There is text of about.</P>
+        <H level={6}>Etymology</H>
+        <P>There is text of etymology.</P>
+        <H level={5}>Practice 02</H>
+        <P>There is hardcoded value.</P>
+        <H level={6}>About</H>
+        <P>There is text of about.</P>
+        <H level={6}>Etymology</H>
+        <P>There is text of etymology.</P>
+        <H level={5}>Practice 03</H>
+        <P>There is updating of lifting state up.</P>
+        <H level={6}>About</H>
+        <P>There is text of about.</P>
+        <H level={6}>Etymology</H>
+        <P>There is text of etymology.</P>
       </Container>
     </Main>
   );

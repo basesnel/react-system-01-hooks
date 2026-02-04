473f9df8f468f90414c0dca68edadad74f8e2eed
basesnel <basesnel@gmail.com>
2026-02-04 10:37:11 +0200

    forwardRef is used for Task09 in the escape-hatches page I

--- src/tasks/EscapeHatchesP01/intendedComponents/MyInputT09/MyInputT09.jsx
+++ src/tasks/EscapeHatchesP01/intendedComponents/MyInputT09/MyInputT09.jsx
@@ -1,7 +1,9 @@
-import { useRef, useEffect } from 'react';
+import { useRef, forwardRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
-// import { FiEdit } from 'react-icons/fi';
+import { FiEdit } from 'react-icons/fi';
 // import { TextInput } from 'components';
+
+import styles from './styles.module.css';
 
 const MyInputT09 = props => {
   const { value, onChange } = props;
@@ -9,11 +11,11 @@ const MyInputT09 = props => {
   const ref = useRef(null);
 
   useEffect(() => {
-    ref.current.focus();
+    if (ref.current) ref.current.focus();
   }, []);
 
   return (
-    <input
+    <TextInput
       ref={ref}
       type="text"
       name="nameT09"
@@ -21,10 +23,47 @@ const MyInputT09 = props => {
       label="Enter your name"
       placeholder="Type your name..."
       onChange={onChange}
-      // icon={<FiEdit />}
+      icon={<FiEdit />}
     />
   );
 };
+
+const TextInput = forwardRef((props, ref) => {
+  const { disabled, type, name, value, label, placeholder, onChange, icon } =
+    props;
+
+  const { field, caption, wrap, input, pictogram } = styles;
+
+  const valueProps = {
+    ...(value === null || onChange === null
+      ? { defaultValue: '', readOnly: true }
+      : { value: value, onChange: onChange }),
+  };
+
+  return (
+    <div className={field}>
+      {label && (
+        <label className={caption} htmlFor={name}>
+          {label}
+        </label>
+      )}
+      <div className={wrap}>
+        <input
+          className={input}
+          id={name}
+          name={name}
+          type={type}
+          disabled={disabled}
+          placeholder={placeholder}
+          autoComplete="off"
+          {...valueProps}
+          ref={ref}
+        />
+        <span className={pictogram}>{icon}</span>
+      </div>
+    </div>
+  );
+});
 
 MyInputT09.propTypes = {
   value: PropTypes.string.isRequired,

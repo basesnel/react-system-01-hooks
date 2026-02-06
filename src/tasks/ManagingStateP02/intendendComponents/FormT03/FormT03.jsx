f3675f6def6fc2c079806e445b095c7cb7cb0ae3
basesnel <basesnel@gmail.com>
2026-02-06 09:26:35 +0200

    Textarea component is applied to task03 in the managing-state page II.

--- src/tasks/ManagingStateP02/intendendComponents/FormT03/FormT03.jsx
+++ src/tasks/ManagingStateP02/intendendComponents/FormT03/FormT03.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 import { MdChat } from 'react-icons/md';
-import { TextArea } from 'components';
+import { Textarea } from 'components';
 
 const FormT03 = () => {
   const [text, setText] = useState('');
   return (
-    <TextArea
-      textAreaName="Task03Text"
-      textAreaValue={text}
+    <Textarea
+      name="Task03Text"
+      value={text}
+      placeholder="Type something..."
       handleChange={e => setText(e.target.value)}
       icon={<MdChat />}
     />

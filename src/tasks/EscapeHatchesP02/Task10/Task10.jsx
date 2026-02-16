e0ff13827d133cc209464ad1360b6b3ac023b3da
basesnel <basesnel@gmail.com>
2026-02-16 15:54:56 +0200

    Select2 is applied to some tasks on escape-hatches-page II.

--- src/tasks/EscapeHatchesP02/Task10/Task10.jsx
+++ src/tasks/EscapeHatchesP02/Task10/Task10.jsx
@@ -1,5 +1,5 @@
 import { MdOutlineLanguage, MdPlace } from 'react-icons/md';
-import { FlexBox, Select, P } from 'components';
+import { FlexBox, Select2, P } from 'components';
 import { useSelectOptions } from './useSelectOptions';
 
 const Task10 = () => {
@@ -11,21 +11,21 @@ const Task10 = () => {
   return (
     <>
       <FlexBox align="flex-start" wrapped="wrap">
-        <Select
-          selectLabel="Pick a planet: "
-          selectName="PlanetT10"
-          selected={planet}
+        <Select2
+          label="Pick a planet: "
+          name="PlanetT10"
+          value={planet}
           list={planetList}
           icon={<MdOutlineLanguage />}
-          onHandleSelect={e => setPlanet(e.target.value)}
+          onChange={e => setPlanet(e.target.value)}
         />
-        <Select
-          selectLabel="Pick a place: "
-          selectName="PlaceT10"
-          selected={place}
+        <Select2
+          label="Pick a place: "
+          name="PlaceT10"
+          value={place}
           list={placeList}
           icon={<MdPlace />}
-          onHandleSelect={e => setPlace(e.target.value)}
+          onChange={e => setPlace(e.target.value)}
         />
       </FlexBox>
       <hr />
